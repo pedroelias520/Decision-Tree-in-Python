3c767b8749bfba32be9de93ba512b4fb005bbbfa
Pedro Elias <epedro520@gmail.com>
2020-09-28 22:32:46 -0300

    upgrade:decision tree completed

--- decision_tree_code.py
+++ decision_tree_code.py
@@ -9,11 +9,6 @@ import pandas as pd
 dataset = pd.read_csv('adult.csv',encoding='latin-1',engine='python')
 feature_cols = dataset.iloc[:, 0:14].columns
 
-import chardet
-with open('adult.csv', 'rb') as rawdata:
-    result = chardet.detect(rawdata.read(100000))
-print(result)
-
 #Cria um array com todos os valores do dataset (sem as colunas)
 X = dataset[feature_cols].values
 #Criar um array com todos os atributos do tipo numérico
@@ -38,7 +33,7 @@ scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, random_state = 0)
 
 #A base de dados não está balanceada, tornando os dados tendenciosos, então deve balancea-la
 from imblearn.under_sampling import RandomUnderSampler
@@ -47,15 +42,29 @@ X_train,y_train = rus.fit_sample(X_train,y_train)
 
 #Criando árvore de decisão
 from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier()
+clf = DecisionTreeClassifier(max_depth=3,criterion='entropy')
 #Treino de modelo
 clf = clf.fit(X_train, y_train)
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(
+        clf,
+        out_file="decision_tree_new.dot",
+        feature_names=feature_cols,
+        class_names=['<=50','>50'],
+        #class_names=base['class'],
+        rounded=True,
+        filled=True
+    )
+
 
 y_pred = clf.predict(X_test)
 from sklearn import metrics
 
 accuracy = metrics.accuracy_score(y_test,y_pred)
 print(metrics.confusion_matrix(y_test, y_pred))
+print(accuracy)
 
 
 
